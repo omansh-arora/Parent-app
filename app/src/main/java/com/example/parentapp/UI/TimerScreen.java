ecac7a640924cb0f8db3ade974a7a59992e61ab4
Omansh <oarora@sfu.ca>
2021-11-04 11:08:02 -0700

    Made timer work in background

--- app/src/main/java/com/example/parentapp/UI/TimerScreen.java
+++ app/src/main/java/com/example/parentapp/UI/TimerScreen.java
@@ -2,6 +2,7 @@ package com.example.parentapp.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -28,13 +29,15 @@ public class TimerScreen extends AppCompatActivity {
 
     private Button buttonTimeSet;
     private EditText editSetTime;
-    private TextView TXTeditSetTime;
+    private TextView TXT_editSetTime;
 
     private CountDownTimer countDownTimer;
 
     private boolean boolTimerRunning;
 
     private long timeLeftMillis = START_TIME_MILLIS;
+
+    private long endTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +57,7 @@ public class TimerScreen extends AppCompatActivity {
 
         buttonTimeSet = findViewById(R.id.timer_btn_setTime);
         editSetTime = findViewById(R.id.timer_inp_timeInp);
-        TXTeditSetTime = findViewById(R.id.timer_txt_timeEnter);
+        TXT_editSetTime = findViewById(R.id.timer_txt_timeEnter);
 
         buttonStartPauseResume.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -120,41 +123,40 @@ public class TimerScreen extends AppCompatActivity {
         START_TIME_MILLIS = (long)i*60*1000;
         timeLeftMillis = (long)i*60*1000;
         updateCountDownText();
-
+        updateButtons();
     }
 
     private void pauseTimer() {
 
         countDownTimer.cancel();
         boolTimerRunning =false;
-        buttonStartPauseResume.setText("Start");
-        buttonReset.setVisibility(View.VISIBLE);
+        updateButtons();
 
     }
 
     private void startTimer() {
+
+        endTime = System.currentTimeMillis() + timeLeftMillis;
 
         countDownTimer = new CountDownTimer(timeLeftMillis, 1000) {
             @Override
             public void onTick(long millisLeftUntilDone) {
                 timeLeftMillis = millisLeftUntilDone;
                 updateCountDownText();
+                updateButtons();
             }
 
             @Override
             public void onFinish() {
                 boolTimerRunning = false;
-                buttonStartPauseResume.setText("Start");
-                buttonStartPauseResume.setVisibility(View.INVISIBLE);
-                buttonReset.setVisibility(View.VISIBLE);
+                updateButtons();
 
             }
         }.start();
 
         boolTimerRunning = true;
         buttonStartPauseResume.setText("pause");
-        buttonReset.setVisibility(View.INVISIBLE);
-        editSetTime.setVisibility(View.INVISIBLE);
+        updateButtons();
 
     }
 
@@ -174,9 +176,84 @@ public class TimerScreen extends AppCompatActivity {
 
         timeLeftMillis = START_TIME_MILLIS;
         updateCountDownText();
-        buttonStartPauseResume.setVisibility(View.VISIBLE);
-        buttonReset.setVisibility(View.VISIBLE);
-        editSetTime.setVisibility(View.VISIBLE);
+        updateButtons();
+
+    }
+
+    private void updateButtons() {
+        if (boolTimerRunning) {
+            buttonReset.setVisibility(View.INVISIBLE);
+            buttonStartPauseResume.setText("Pause");
+            buttonTimeSet.setVisibility(View.INVISIBLE);
+            editSetTime.setVisibility(View.INVISIBLE);
+            TXT_editSetTime.setVisibility(View.INVISIBLE);
+        } else {
+            buttonStartPauseResume.setText("Start");
+            buttonStartPauseResume.setVisibility(View.INVISIBLE);
+            buttonTimeSet.setVisibility(View.VISIBLE);
+            editSetTime.setVisibility(View.VISIBLE);
+            TXT_editSetTime.setVisibility(View.VISIBLE);
+
+            if (timeLeftMillis < 1000) {
+
+            } else {
+                buttonStartPauseResume.setVisibility(View.VISIBLE);
+            }
+
+            if (timeLeftMillis < START_TIME_MILLIS) {
+                buttonReset.setVisibility(View.VISIBLE);
+            } else {
+                buttonReset.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    @Override
+    protected void onStop() {
+
+        SharedPreferences prefs = this.getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putLong("millisLeft", timeLeftMillis);
+        editor.putBoolean("timerRunning", boolTimerRunning);
+        editor.putLong("endTime", endTime);
+
+        editor.apply();
+
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onStart() {
+
+
+        SharedPreferences prefs = this.getSharedPreferences("prefs", MODE_PRIVATE);
+
+        timeLeftMillis = prefs.getLong("millisLeft",timeLeftMillis);
+        boolTimerRunning = prefs.getBoolean("timerRunning", false);
+
+
+        updateCountDownText();
+        updateButtons();
+
+        if (boolTimerRunning) {
+            endTime = prefs.getLong("endTime", 0);
+            timeLeftMillis = endTime - System.currentTimeMillis();
+
+            if (timeLeftMillis < 0) {
+                timeLeftMillis = 0;
+                boolTimerRunning = false;
+                updateCountDownText();
+                updateButtons();
+            } else {
+                startTimer();
+            }
+        }
+        super.onStart();
 
     }
 }
